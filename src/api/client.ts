ec37feccce448352f092120bb8753d5e16fe31cd
elmarAbdullayev <114047390+elmarAbdullayev@users.noreply.github.com>
2026-03-21 14:41:35 +0100

    first commit

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,7 +5,7 @@ import axios from "axios";
 
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://truthtalk-backend-production.up.railway.app/", 
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",  
   headers: {
     "Content-Type": "application/json",
   },
